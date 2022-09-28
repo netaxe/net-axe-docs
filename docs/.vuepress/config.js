bcf30dce1f803567ea18c20b297449a079b5080c
unknown <xhweng@iflytek.com>
2022-09-28 17:46:07 +0800

    theme-update-new

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -21,9 +21,38 @@ module.exports = {
     // 使用的主题
     theme: 'vuepress-theme-vdoing',
     themeConfig: {
-        logo: '/favicon.ico',  //网页顶端导航栏左上角的图标
+        logo: '',  //网页顶端导航栏左上角的图标
         hostname:"https://netaxe.github.io/",
-        author:"NetAxe",
+        author: {
+            name: 'netaxe', // 必需
+            link: 'https://gitee.com/iflytek/NetAxe.git', // 可选的
+          },
+          // 社交图标 (显示于博主信息栏和页脚栏。内置图标：https://doc.xugaoyi.com/pages/a20ce8/#social)
+    social: {
+        // iconfontCssFile: '//at.alicdn.com/t/xxx.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自己添加。阿里图片库：https://www.iconfont.cn/
+        icons: [
+          {
+            iconClass: 'icon-youjian',
+            title: '发邮件',
+            link: 'netaxe@163.com',
+          },
+          {
+            iconClass: 'icon-github',
+            title: 'GitHub',
+            link: 'https://github.com/iflytek/NetAxe.git',
+          },
+          {
+            iconClass: 'icon-erji',
+            title: '听音乐',
+            link: 'https://music.163.com/#/playlist?id=755597173',
+          },
+        ],
+      },
+        footer: {
+            createYear: 2022, // 博客创建年份
+            copyrightInfo:
+              'NetAxe| <a href="https://gitee.com/iflytek/NetAxe/blob/master/LICENSE" target="_blank">Apache License2.0</a>', // 博客版权信息、备案信息等，支持a标签或换行标签</br>
+          },
         locales: {
             "/": {
               // 设置正在使用的语言
